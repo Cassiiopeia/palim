55205bb821ae2b38fbce268002c10a009e76582a
SUH SAECHAN <chan4760@gmail.com>
2026-07-29 17:19:48 +0900

    chore : Spring Boot 4.1.0 프로젝트 초기 구성 (Java 25 / Gradle 9.5.1)

---
+++ src/test/java/kr/suhsaechan/palim/PalimApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.suhsaechan.palim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
